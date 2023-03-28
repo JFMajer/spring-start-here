07c1da8b2b0b549a215e44acc6de5e4ff2bc28fa
JFMajer <jakubmajer@live.com>
2023-03-28 18:03:13 +0200

    added spring framework to pom and added Component annotations

--- interfaces-no-framework/src/main/java/main/Main.java
+++ interfaces-no-framework/src/main/java/main/Main.java
@@ -1,6 +1,8 @@
 package main;
 
+import configuration.ProjectConfiguration;
 import model.Comment;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import proxies.EmailNotificationproxy;
 import repositories.CommentRepository;
 import repositories.DBCommentRepository;
@@ -9,14 +11,13 @@ import services.CommentService;
 public class Main {
 
         public static void main(String[] args) {
-            var commentRepository = new DBCommentRepository();
-            var commentNotificationProxy = new EmailNotificationproxy();
+            var context = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
 
-            var commentService = new CommentService(commentRepository, commentNotificationProxy);
             var comment = new Comment();
-            comment.setAuthor("John Doe");
             comment.setText("This is a comment");
+            comment.setAuthor("Jakub");
 
+            var commentService = context.getBean(CommentService.class);
             commentService.publishComment(comment);
 
         }
