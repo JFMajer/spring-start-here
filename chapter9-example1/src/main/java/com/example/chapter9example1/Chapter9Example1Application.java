8ef44d9a6cb36b310be688cf9a79f1e92d892ae0
JFMajer <jakubmajer@live.com>
2023-03-31 20:22:44 +0200

    request scope

---
+++ chapter9-example1/src/main/java/com/example/chapter9example1/Chapter9Example1Application.java
@@ -0,0 +1,13 @@
+package com.example.chapter9example1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter9Example1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter9Example1Application.class, args);
+	}
+
+}
---
+++ chapter9-example1/src/main/java/com/example/chapter9example1/controllers/LoginController.java
@@ -0,0 +1,42 @@
+package com.example.chapter9example1.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import com.example.chapter9example1.processors.LoginProcessor;
+
+@Controller
+public class LoginController {
+
+    private final LoginProcessor lp;
+
+    public LoginController(LoginProcessor lp) {
+        this.lp = lp;
+    }
+
+    @GetMapping("/")
+    public String loginGet() {
+        return "login.html";
+    }
+
+    @PostMapping("/")
+    public String loginPost(
+            @RequestParam String username,
+            @RequestParam String password,
+            Model model
+    ) {
+        lp.setUsername(username);
+        lp.setPassword(password);
+        boolean loggedIn = lp.login();
+
+        if (loggedIn) {
+            model.addAttribute("message", "You are logged in!");
+        } else {
+            model.addAttribute("message", "You are not logged in!");
+        }
+
+        return "login.html";
+    }
+}
